95791899b7db74bdbe69c6103f7a987d473aac98
newtondotcom <asphalt9fr@gmail.com>
2023-11-27 21:29:39 +0100

    rm typo

--- worker/app.py
+++ worker/app.py
@@ -23,4 +23,3 @@ def callback(ch, method, properties, body):
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
 channel.start_consuming()
-view raw

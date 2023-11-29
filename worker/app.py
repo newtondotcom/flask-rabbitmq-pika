8373ff233d22095de86c6d851e6a0838a616a545
newtondotcom <asphalt9fr@gmail.com>
2023-11-29 17:36:14 +0100

    working worker with time before start

--- worker/app.py
+++ worker/app.py
@@ -1,25 +1,47 @@
 import pika
+import time
+import json
 
-print(' Connecting to server ...')
-
-try:
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="127.0.0.1",port=5672))
-except pika.exceptions.AMQPConnectionError as exc:
-    print("Failed to connect to RabbitMQ service. Message wont be sent.")
-    exit()
-    
-channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
-
-print(' Waiting for messages...')
-
+# RabbitMQ connection parameters
+rabbitMQUrl = 'amqp://rabbitmq'
 
 def callback(ch, method, properties, body):
-    print(" Received %s" % body.decode())
-    print(" Done")
+    try:
+        task = json.loads(body)
+        # Process the task here
+        print(f"Received task: {task}")
+        # Replace this with your actual task processing logic
+        time.sleep(30)
+        # Acknowledge that the task is processed
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(f"Error processing task: {e}")
+        # You can handle errors or requeue the task if needed
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+def worker():
+    time.sleep(30)  # Wait for RabbitMQ to start
+    try:
+        connection = pika.BlockingConnection(pika.URLParameters(rabbitMQUrl))
+        channel = connection.channel()
+        
+        exchange = 'tasks'
+        queue = 'worker_queue'  # You can name your queue appropriately
+        
+        # Declare a queue
+        channel.queue_declare(queue=queue,durable=True)
+        
+        # Bind the queue to the exchange
+        channel.queue_bind(exchange=exchange, queue=queue)
+        
+        # Set up the callback function to handle incoming tasks
+        channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=False)
+        
+        print('Worker is waiting for tasks. To exit, press CTRL+C')
+        
+        # Start consuming tasks
+        channel.start_consuming()
+    except Exception as e:
+        print(f"Error in worker: {e}")
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
-channel.start_consuming()
+if __name__ == '__main__':
+    worker()
